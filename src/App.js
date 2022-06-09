51977d23c902956e7df68073e0198a34ea247d2c
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-09 15:39:32 +0300

    closing the function with }

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import React from 'react';
+function App() {
+  return (
+      <div>
+      </div>
+  )};
 
-import './App.css';
-function MyList(props) {
-  const items = props.items;
-  const listItems = items.map((item) =>
-    <li>{item}</li>
-  );
-  function App() {
-    return (
-      <ul>{listItems}</ul>
-    );
-  }
-  ReactDOM.render(
-    <MyList items={items} />,
-    document.getElementById('root')
-  );
-}
 export default App;
