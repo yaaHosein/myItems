95b3d3a8a55eadfb2c2930b6f08052c5b0a64f6f
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-12 07:42:44 +0300

    removing some codes and adding a span tag

--- src/EntryList.js
+++ src/EntryList.js
@@ -1,18 +1,7 @@
 function EntryList() {
 
     return (
-        <div>
-            <h6 className="yu" style="color:blue;">My List</h6>
-            <form>
-                <label>
-                    <input type="text" placeholder="Enter Something"> </input>
-                </label>
-            </form>
-            <div class="w3-padding w3-xlarge w3-teal">
-                <i class="glyphicon glyphicon-trash"></i>
-
-            </div>
-        </div>
+        <span></span>
     )
 }
 
