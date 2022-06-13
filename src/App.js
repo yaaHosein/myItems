0d300138bfe77b04a94c506ce9f6cd58e8644a5e
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-13 23:55:21 +0300

    trying to make an entry list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import EntryList from "./EntryList.js";
-import data from "./data.json";
+import EntryList from "./EntryList";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <EntryList fruit="Apple" />
-      <EntryList fruit="Banana" />
-      <EntryList fruit="Orange" />
-    </div>
-  );
-
-}
-const items = data.map(function (item) {
   return
-  <EntryList
-    fruit={item.fruit}
-  />
-  return (
-    <div>
-      {items}
+  (
+    <div className="App">
+      <EntryList fruits={["carrot"]} />
+
     </div>
   )
-});
+}
 
 export default App;
 
