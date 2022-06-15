ffc094bf145f1bf3cbe3283d1cf14ea3dad20488
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-15 19:11:13 +0300

    making return and ( in one line

--- src/EntryList.js
+++ src/EntryList.js
@@ -1,12 +1,10 @@
-function EntryList(props) {
 
-    return
-    (
+function EntryList(props) {
+    return (
         <ul>
             {fruits.map(function (fruit) {
                 return <li>{props.fruits.map}</li>
             })}
         </ul>)
-
 }
 export default EntryList;
