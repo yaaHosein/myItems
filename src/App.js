f9d72f5aa3662029d2a59fbd8b7edb29e09d6cb5
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-15 19:11:24 +0300

    making return and ( in one line

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import EntryList from "./EntryList";
 import './App.css';
 const fruits = ["carrot"]
 function App() {
-  return
-  (
+  return (
     <div className="App">
       <EntryList fruits={fruits} />
-
     </div>
   )
 }
-
 export default App;
 
