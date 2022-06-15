b578fba9051ee1dff31b621e8aafd5746da6ade3
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-15 19:23:11 +0300

    adding <></>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import EntryList from "./EntryList";
 import './App.css';
-const fruits = ["carrot"]
+const fruits = ["carrot"];
 function App() {
   return (
+    <>
     <div className="App">
       <EntryList fruits={fruits} />
     </div>
+    </>
   )
 }
 export default App;
