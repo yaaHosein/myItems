c2690f57ca428d1d0dc8685462ddc8ce9ba5f891
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-28 18:04:35 +0300

    adding the needed codes for deleting items and filelds

--- src/EntryList.js
+++ src/EntryList.js
@@ -9,7 +9,6 @@ function EntryList(props) {
             // adding unordered list tag and looping arround items by using map function //
             return <li key={index}>
                 <input type="text" value={item} onChange={function (event) {
-
                     // inside list items tag adding input tag for text input and assinig its default value to item; also adding onChange function for changing the fields contents //
                     const newValue = event.target.value;
                     // defining newValue getting the new Vakue of the input
@@ -19,7 +18,14 @@ function EntryList(props) {
                     // updating the new array value using the index paramter
                     setitems(itemArrayCopy);
                     // setting setItems fun for setting the new array as a vlue 
-                }} /></li>
+                }} />
+                <button onClick={function () {
+                    const itemArrayCopy = [...items];
+                    itemArrayCopy.splice(1, 1);
+                    setitems(itemArrayCopy);
+                }}>X</button>
+
+            </li>
         })}</ul>
         <button onClick={function () {
             const itemArrayCopy = [...items];
