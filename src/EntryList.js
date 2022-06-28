4bd78e6f60f57c2133aae51b04773dbfba5e8953
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-28 16:23:17 +0300

    making a list item and using useState hook for it

--- src/EntryList.js
+++ src/EntryList.js
@@ -1,14 +1,27 @@
+import { useState } from "react"
+// importi useState hook from react liberrary//
 function EntryList(props) {
-    console.log(props) 
-    return (
-        <>
-            <ul> 
-            {fruits.map(function (fruit) {
-                return <li>{props.fruits.map}</li>
-            })}
-            </ul>
-        </>
-    )
+    //defining EntryList function//
+    const [items, setitems] = useState(props.items)
+    // defining useState var and assing 
+    return (<>
+        <ul>{items.map(function (item, index) {
+            // adding unordered list tag and looping arround items by using map function //
+            return <li>
+                <input type="text" value={item} onChange={function (event) {
+                    // inside list items tag adding input tag for text input and assinig its default value to item; also adding onChange function for changing the fields contents //
+                    const newValue = event.target.value;
+                    // defining newValue getting the new Vakue of the input
+                    const itemArrayCopy = [...items];
+                    // defining a new var for cloning the items 
+                    itemArrayCopy[index] = newValue;
+                    // updating the new array value using the index paramter
+                    setitems(itemArrayCopy);
+                    // setting setItems fun for setting the new array as a vlue 
+                }} /></li>
+        })}</ul>
+    </>
+    );
 }
-
 export default EntryList;
+
