f720dfaa54e4bc483e4929e017f219fcfc20e952
yaaHosein <layali.14.peshawar@hotmail.com>
2022-06-30 04:30:50 +0300

    adding All Clear button for clearing all items

--- src/EntryList.js
+++ src/EntryList.js
@@ -4,38 +4,51 @@ function EntryList(props) {
     //defining EntryList function//
     const [items, setitems] = useState(props.items)
     // defining useState var and assing
-    return (<>
-        <ul>{items.map(function (item, index) {
-            // adding unordered list tag and looping arround items by using map function //
-            return <li key={index}>
-                <input type="text" value={item} onChange={function (event) {
-                    // inside list items tag adding input tag for text input and assinig its default value to item; also adding onChange function for changing the fields contents //
-                    const newValue = event.target.value;
-                    // defining newValue getting the new Vakue of the input
-                    const itemArrayCopy = [...items];
-                    // defining a new var for cloning the items 
-                    itemArrayCopy[index] = newValue;
-                    // updating the new array value using the index paramter
-                    setitems(itemArrayCopy);
-                    // setting setItems fun for setting the new array as a vlue 
-                }} />
-                <button onClick={function () {
-                    const itemArrayCopy = [...items];
-                    itemArrayCopy.splice(1, 1);
-                    setitems(itemArrayCopy);
-                }}>X</button>
 
-            </li>
-        })}</ul>
+    return (<>
+        <ul>
+            {items.map(function (item, index) {
+                // adding unordered list tag and looping arround items by using map function //
+                return <li key={index}>
+                    <input type="text" value={item} onChange={function (event) {
+                        // inside list items tag adding input tag for text input and assinig its default value to item; also adding onChange function for changing the fields contents //
+                        const newValue = event.target.value;
+                        // defining newValue getting the new Vakue of the input
+                        const itemArrayCopy = [...items];
+                        // defining a new var for cloning the items 
+                        itemArrayCopy[index] = newValue;
+                        // updating the new array value using the index paramter
+                        setitems(itemArrayCopy);
+                        // setting setItems fun for setting the new array as a vlue
+                    }} />
+                    <button onClick={function () {
+                        const itemArrayCopy = [...items];
+                        itemArrayCopy.splice(1, 1);
+                        setitems(itemArrayCopy);
+                        console.log("mm", setitems())
+                        console.log("ff", setitems(itemArrayCopy))
+                        console.log("ww", setitems(itemArrayCopy) !== setitems())
+                    }}>X</button>
+
+                </li>
+            })}</ul>
         <button onClick={function () {
             const itemArrayCopy = [...items];
             itemArrayCopy.push("");
-            setitems(itemArrayCopy);   
+            setitems(itemArrayCopy);
         }}>Add</button>
 
         <button onClick={function () {
             setitems([]);
         }}>Remove All</button>
+
+        <button onClick={function () {
+            const clearedItems = items.map(function (item) {
+                return "";
+            })
+            setitems(clearedItems);
+
+        }}>Clear All</button>
 
     </>)
 }
