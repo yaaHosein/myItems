4fbf33a13d1a7c4a9da418e960a32cfd5c095464
yaaHosein <layali.14.peshawar@hotmail.com>
2022-07-04 12:14:28 +0300

    saving in local sotrage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import EntryList from "./EntryList";
-import './App.css';
-const items = ["carrot ", "Orange ", "Banana "];
+import "./App.css";
+let items = ["carrot ", "Orange ", "Banana "];
+if (localStorage.getItem("foo")) {
+  items = JSON.parse(localStorage.getItem("foo"));
+}
 function App() {
   return (
     <>
       <div className="App">
-        <EntryList items={items} onChange={function (newItems) {
-          const StringedItems = JSON.stringify(newItems);
-          localStorage.setItem("foo", StringedItems)
-        }} />
-    </div>
+        <EntryList
+          items={items}
+          onChange={function (newItems) {
+            const StringedItems = JSON.stringify(newItems);
+            localStorage.setItem("foo", StringedItems);
+          }}
+        />
+      </div>
     </>
-  )
+  );
 }
 export default App;
