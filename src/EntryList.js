029570b97b4ff86152ea03db52fa8afaf3f83dea
yaaHosein <layali.14.peshawar@hotmail.com>
2022-08-10 15:37:39 +0300

    removing All Clear a Remove All buttons when we do not have any fields

--- src/EntryList.js
+++ src/EntryList.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 // importing useState hook from react literary//
 function EntryList(props) {
   //defining EntryList function//
     const [items, setitems] = useState(props.items);
   // defining useState var and assigning it to useState hook
+
     useEffect(() => {
         props.onChange(items);
     }, [items]);
@@ -12,12 +13,12 @@ function EntryList(props) {
     }, [props.items]);
     let allEmpty = true;
 
-    for (let i = 0; i > items.length; i++) {
-        if (items !== "") {
-            allEmpty = false;
-            break;
-        }
-    }
+  // for (let i = 0; i > items.length; i++) {
+  //     if (items !== "") {
+  //         allEmpty = false;
+  //         break;
+  //     }
+  // }
 
     return (
         <>
@@ -25,20 +26,20 @@ function EntryList(props) {
                 {items.map(function (item, index) {
           // adding unordered list tag and looping around items by using map function //
 
-                    return (
-                        <li key={index}>
-                            <input
-                                type="text"
-                                value={item}
-                                onChange={function (event) {
-                                    // inside list items tag adding input tag for text input and assigning its default value to item; also adding onChange function for changing the fields contents //
-                                    const newValue = event.target.value;
-                                    // defining newValue getting the new Value of the input
-                                    const itemArrayCopy = [...items];
-                                    // defining a new var for cloning the items
-                                    itemArrayCopy[index] = newValue;
-                                    // updating the new array value using the index parameter
-                                    setitems(itemArrayCopy);
+            return (
+                <li key={index}>
+                    <input
+                        type="text"
+                        value={item}
+                        onChange={function (event) {
+                            // inside list items tag adding input tag for text input and assigning its default value to item; also adding onChange function for changing the fields contents //
+                            const newValue = event.target.value;
+                            // defining newValue getting the new Value of the input
+                            const itemArrayCopy = [...items];
+                            // defining a new var for cloning the items
+                            itemArrayCopy[index] = newValue;
+                            // updating the new array value using the index parameter
+                            setitems(itemArrayCopy);
                   // setting setItems for setting the new array as a value
                         }}
                     />
@@ -62,41 +63,44 @@ function EntryList(props) {
                         Clear
                     </button>
                 </li>
-                    );
-                })}
-            </ul>
-            <button
-                onClick={function () {
-                    const itemArrayCopy = [...items];
-                    itemArrayCopy.push("");
-                    setitems(itemArrayCopy);
-                }}
-            >
-                Add
-            </button>
-            <button
-                onClick={function () {
-                    const emptyArry = [];
-                    setitems(emptyArry);
-                }}
-            >
-                Remove All
-            </button>
-
-            <button
-                disabled={items.every(function (item) {
-                    return item.trim() === "";
-                })}
-                onClick={function () {
-                    const clearedItems = items.map(function () {
-                        return "";
-                    });
-                    setitems(clearedItems);
-                }}
-            >
-                Clear All
-            </button>
-        </>
-    );
+            );
+        })}
+          </ul>
+          <button
+              onClick={function () {
+                  const itemArrayCopy = [...items];
+                  itemArrayCopy.push("");
+                  setitems(itemArrayCopy);
+              }}
+          >
+              Add
+          </button>
+          {items.length !== 0 ? (
+              <button
+                  onClick={function () {
+                      const emptyArry = [];
+                      setitems(emptyArry);
+                  }}
+              >
+                  Remove All
+              </button>
+          ) : null}
+          {items.length !== 0 ? (
+              <button
+                  disabled={items.every(function (item) {
+                      return item.trim() === "";
+                  })}
+                  onClick={function () {
+                      const clearedItems = items.map(function () {
+                          return "";
+                      });
+                      setitems(clearedItems);
+                  }}
+              >
+                  Clear All
+              </button>
+          ) : null}
+      </>
+  );
 }
 export default EntryList;
