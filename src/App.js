32383eb74baf8e0584ef8c1ec0c56fe15f0f79ec
yaaHosein <layali.14.peshawar@hotmail.com>
2022-10-27 08:15:54 +0330

    h

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React from 'react';
 
-import './App.css';
-function MyList(props) {
-  const items = props.items;
-  const listItems = numbers.map((item) =>
-    <li>{item}</li>
-  );
-  function App() {
-    return (
-      <ul>{listItems}</ul>
-    );
-  }
-  const items = [item1, item2, item3, item4, item5];
-  ReactDOM.render(
-    <MyList items={items} />,
-    document.getElementById('root')
+import EntryList from "./EntryList";
+import "./App.css";
+import { useEffect, useState } from "react";
+let items = [];
+
+if (localStorage.getItem("foo")) {
+  console.log(localStorage.getItem("foo"));
+  items = JSON.parse(localStorage.getItem("foo"));
+}
+
+function App() {
+  let [xxx, setXxx] = useState([]);
+  console.log(xxx);
+  console.log(items);
+  console.log(localStorage.getItem("foo"));
+  useEffect(() => {
+    if (localStorage.getItem("foo") !== null) {
+      setXxx(items);
+      console.log(setXxx);
+    } else {
+      fetch("http://localhost:9300/")
+        .then((response) => {
+          response.json().then((parsedServerData) => {
+            setXxx(parsedServerData);
+            console.log(response);
+          });
+          console.log(response);
+        })
+
+        .catch((error) => {
+          console.log("error==>", error);
+        });
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <EntryList
+        items={xxx}
+        onChange={function (newItems) {
+          if (Array.isArray(newItems)) {
+            const json = JSON.stringify(newItems);
+            localStorage.setItem("foo", json);
+            // save new items in local storage
+          }
+        }}
+      />
+    </div>
   );
 }
 
 export default App;
+
